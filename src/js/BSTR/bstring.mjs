53b86c88159e96330adba419ae836990288fc26e
unit-404 <elviras9t@gmail.com>
2023-10-11 20:49:15 +0000

    bit better naming

--- src/js/BSTR/bstring.mjs
+++ src/js/BSTR/bstring.mjs
@@ -4,7 +4,7 @@
 // "bytes" - Uint8Array binary data
 // "base64" - base64 representation
 
-//
+// TODO! needs to rewrite...
 export class TString {
     //
     constructor(from = "", fromCoding = "utf8", toCoding = "utf8") {
@@ -25,35 +25,35 @@ export class TString {
 }
 
 //
-const $dec = new TextDecoder();
-const $enc = new TextEncoder();
+const utf8_dec = new TextDecoder();
+const utf8_enc = new TextEncoder();
 
 //
-const $raw_base64 = btoa;
-const $base64_raw = atob;
+const raw_base64 = btoa;
+const base64_raw = atob;
 
 //
-const $codes_str    = (from)=>{ return String.fromCodePoint(...from); };
-const $utf16_shorts = (from)=>{ return Uint16Array.from(from.split("").map((e)=>(e.codePointAt(0)))) };
+const codes_str    = (from)=>{ return String.fromCodePoint(...from); };
+const utf16_shorts = (from)=>{ return Uint16Array.from(from.split("").map((e)=>(e.codePointAt(0)))) };
 
 //
-const $str_utf8 = (from)=>{ return $enc.encode(from) };
-const $utf8_str = (from)=>{ return $dec.decode(from) };
+const str_utf8 = (from)=>{ return utf8_enc.encode(from) };
+const utf8_str = (from)=>{ return utf8_dec.decode(from) };
 
 //
-const $utf8_raw = (from) => { return unescape(encodeURIComponent(from)) };
-const $raw_utf8 = (from) => { return decodeURIComponent(escape(from)) };
+const utf8_raw = (from) => { return unescape(encodeURIComponent(from)) };
+const raw_utf8 = (from) => { return decodeURIComponent(escape(from)) };
 
 //
-const $raw_bytes = (from) => { return Uint8Array.from(from, (m) => m.codePointAt(0)); };
-const $bytes_raw = $codes_str;
+const raw_bytes = (from) => { return Uint8Array.from(from, (m) => m.codePointAt(0)); };
+const bytes_raw = codes_str;
 
 //
-const $as_shorts = (bytes) => { return new Uint16Array(bytes.buffer, bytes.byteOffset); }
-const $as_bytes  = (shorts) => { return new Uint8Array(shorts.buffer, shorts.byteOffset); }
+const as_shorts = (src) => { return new Uint16Array(src.buffer, src.byteOffset); }
+const as_bytes  = (src) => { return new Uint8Array(src.buffer, src.byteOffset); }
 
 //
-class _Base_ {
+class Rules$ {
     constructor($name, $asMap) {
         this.$name = $name;
         this.$asMap = $asMap;
@@ -72,53 +72,53 @@ class _Base_ {
 }
 
 //
-export const Base64 = new _Base_("base64", new Map([
-    ["raw"   , (raw) => { return $base64_raw(raw); }],
-    ["utf8"  , (raw) => { return $raw_utf8($base64_raw(raw)); }],
-    ["utf16" , (raw) => { return $codes_str($as_shorts($raw_bytes($base64_raw(raw)))); }],
-    ["bytes" , (raw) => { return $raw_bytes($base64_raw(raw)); }],
-    ["shorts", (raw) => { return $as_shorts($raw_bytes($base64_raw(raw))) }],
+export const Base64 = new Rules$("base64", new Map([
+    ["raw"   , (src) => { return base64_raw(src); }],
+    ["utf8"  , (src) => { return raw_utf8(base64_raw(src)); }],
+    ["utf16" , (src) => { return codes_str(as_shorts(raw_bytes(base64_raw(src)))); }],
+    ["bytes" , (src) => { return raw_bytes(base64_raw(src)); }],
+    ["shorts", (src) => { return as_shorts(raw_bytes(base64_raw(src))) }],
 ]));
 
 //
-export const Raw = new _Base_("raw", new Map([
-    ["utf8"  , (raw) => { return $raw_utf8(raw); }],
-    ["utf16" , (raw) => { return $codes_str($as_shorts($raw_bytes(raw))); }],
-    ["base64", (raw) => { return $raw_base64(raw); }],
-    ["bytes" , (raw) => { return $raw_bytes(raw); }],
-    ["shorts", (raw) => { return $as_shorts($raw_bytes(raw)) }],
+export const Raw = new Rules$("raw", new Map([
+    ["utf8"  , (src) => { return raw_utf8(src); }],
+    ["utf16" , (src) => { return codes_str(as_shorts(raw_bytes(src))); }],
+    ["base64", (src) => { return raw_base64(src); }],
+    ["bytes" , (src) => { return raw_bytes(src); }],
+    ["shorts", (src) => { return as_shorts(raw_bytes(src)) }],
 ]));
 
 //
-export const Shorts = new _Base_("shorts", new Map([
-    ["utf16" , (shorts) => { return $codes_str(shorts); }],
-    ["base64", (shorts) => { return $raw_base64($bytes_raw($as_bytes(shorts))); }],
-    ["bytes" , (shorts) => { return $as_bytes(shorts); }],
-    ["raw"   , (shorts) => { return $bytes_raw($as_bytes(shorts)); }],
+export const Shorts = new Rules$("shorts", new Map([
+    ["utf16" , (src) => { return codes_str(src); }],
+    ["base64", (src) => { return raw_base64(bytes_raw(as_bytes(src))); }],
+    ["bytes" , (src) => { return as_bytes(src); }],
+    ["raw"   , (src) => { return bytes_raw(as_bytes(src)); }],
 ]));
 
 //
-export const Bytes = new _Base_("bytes", new Map([
-    ["utf8"     , (bytes) => { return $utf8_str(bytes); }],
-    ["utf16"    , (bytes) => { return $codes_str($as_shorts(bytes)); }],
-    ["base64"   , (bytes) => { return $raw_base64($bytes_raw(bytes)); }],
-    ["raw"      , (bytes) => { return $bytes_raw(bytes); }],
-    ["shorts"   , (bytes) => { return $as_shorts(bytes); }],
+export const Bytes = new Rules$("bytes", new Map([
+    ["utf8"     , (src) => { return utf8_str(src); }],
+    ["utf16"    , (src) => { return codes_str(as_shorts(src)); }],
+    ["base64"   , (src) => { return raw_base64(bytes_raw(src)); }],
+    ["raw"      , (src) => { return bytes_raw(src); }],
+    ["shorts"   , (src) => { return as_shorts(src); }],
 ]));
 
 //
-export const UTF16 = new _Base_("utf16", new Map([
-    ["base64"   , (native) => { return $raw_base64($bytes_raw($as_bytes($utf16_shorts(native)))); }],
-    ["bytes"    , (native) => { return $as_bytes($utf16_shorts(native)); }],
-    ["raw"      , (native) => { return $bytes_raw($as_bytes($utf16_shorts(native))); }],
-    ["shorts"   , (native) => { return $utf16_shorts(native); }],
+export const UTF16 = new Rules$("utf16", new Map([
+    ["base64"   , (src) => { return raw_base64(bytes_raw(as_bytes(utf16_shorts(src)))); }],
+    ["bytes"    , (src) => { return as_bytes(utf16_shorts(src)); }],
+    ["raw"      , (src) => { return bytes_raw(as_bytes(utf16_shorts(src))); }],
+    ["shorts"   , (src) => { return utf16_shorts(src); }],
 ]));
 
 //
-export const UTF8 = new _Base_("utf8", new Map([
-    ["base64"   , (native) => { return $raw_base64($utf8_raw(native)); }],
-    ["raw"      , (native) => { return $utf8_raw(native); }],
-    ["bytes"    , (native) => { return $str_utf8(native); }],
+export const UTF8 = new Rules$("utf8", new Map([
+    ["base64"   , (src) => { return raw_base64(utf8_raw(src)); }],
+    ["raw"      , (src) => { return utf8_raw(src); }],
+    ["bytes"    , (src) => { return str_utf8(src); }],
 ]));
 
 //
@@ -132,11 +132,11 @@ export const DataMap = {
 };
 
 //
-export const Text = new _Base_("text", new Map([]));
+export const Text = new Rules$("text", new Map([]));
 
 // assign an library
 Object.entries(DataMap).map(([$pfx, $lib])=>{
-    for (const $ of $lib.$asMap) { Text.$asMap.set($pfx + "_"   + $[0], $[1]); }
+    for (const $ of $lib.$asMap) { Text.$asMap.set($pfx + "_" + $[0], $[1]); }
 });
 
 //
