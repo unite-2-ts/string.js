caa13851c9bd24c63ee3a874205c0f7cb3078589
unit-404 <conway24b@gmail.com>
2024-10-28 21:01:34 +0700

    reform

---
+++ src/index.mjs
@@ -0,0 +1,5 @@
+export * from "./$js$/String.mjs";
+
+//
+import _def_ from "./$js$/String.mjs";
+export default _def_;
--- src/js/index.mjs
+++
@@ -1,5 +0,0 @@
-export * from "./BSTR/bstring.mjs";
-
-//
-import _def_ from "./BSTR/bstring.mjs";
-export default _def_;
